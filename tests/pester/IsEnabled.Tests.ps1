df4782b834abca6c776afe7ae63b50f819293c68
Timothy Addison <git@tjaddison.com>
2016-12-30 15:12:47 +0000

    Add test for IsEnabled

---
+++ tests/pester/IsEnabled.Tests.ps1
@@ -0,0 +1,13 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "IsEnabled" {
+    It "should execute enabled tasks" {
+        IsEnabled($true) | Should Be 1
+    }
+
+    It "should not execute disabled tasks" {
+        IsEnabled($false) | Should Be 0
+    }
+}
---
+++ tests/pester/IsEnabled.ps1
@@ -0,0 +1,19 @@
+﻿function IsEnabled($enabled) {
+    # Cleanup
+    $query = "delete from scheduler.task; truncate table scheduler.taskexecution;"
+    Invoke-Sqlcmd -ServerInstance . -Database tsqlscheduler -Query $query
+
+    # Create task
+    $query = "insert into scheduler.task (identifier, tsqlcommand, starttime, frequencytype, frequencyinterval, notifyonfailureoperator, IsEnabled) values ('IsEnabledExecution', 'select @@servername', '00:00', 1, 0, 'Test Operator', '$enabled');"
+    Invoke-Sqlcmd -ServerInstance . -Database tsqlscheduler -Query $query
+
+    # Execute task
+    $query = "exec scheduler.ExecuteTask @identifier = 'IsEnabledExecution';"
+    Invoke-Sqlcmd -ServerInstance . -Database tsqlscheduler -Query $query | out-null
+
+    # Verify task completes
+    $query = "select count(*) as ExecutionCount from scheduler.Task as t join scheduler.TaskExecution as te on te.TaskId = t.TaskId where t.Identifier = 'IsEnabledExecution'"
+    $result = Invoke-Sqlcmd -ServerInstance . -Database tsqlscheduler -Query $query
+
+    return $result.ExecutionCount
+}
