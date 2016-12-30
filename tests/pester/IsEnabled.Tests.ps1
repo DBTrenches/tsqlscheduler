de6d076999c3c1f8534de92012d50f070de53934
Timothy Addison <git@tjaddison.com>
2016-12-30 15:32:06 +0000

    Add helpers and refactor code into test

--- tests/pester/IsEnabled.Tests.ps1
+++ tests/pester/IsEnabled.Tests.ps1
@@ -1,13 +1,17 @@
-﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-. "$here\$sut"
+﻿. .\TestHelpers.ps1
 
 Describe "IsEnabled" {
     It "should execute enabled tasks" {
-        IsEnabled($true) | Should Be 1
+        CleanupTasksAndExecutions
+        InsertTask -IsEnabled $true
+        ExecuteTask  
+        GetTaskExecutionCount | Should be 1
     }
 
     It "should not execute disabled tasks" {
-        IsEnabled($false) | Should Be 0
+        CleanupTasksAndExecutions
+        InsertTask -IsEnabled $false
+        ExecuteTask  
+        GetTaskExecutionCount | Should be 0
     }
 }
