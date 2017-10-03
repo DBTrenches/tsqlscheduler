79eb00064a1dcc17d723271ef316e9011146a6d3
Timothy Addison <tim@tjaddison.com>
2017-10-03 10:15:13 +0100

    Add session_id to currently executing tasks

--- src/Views/CurrentlyExecutingTasks.sql
+++ src/Views/CurrentlyExecutingTasks.sql
@@ -4,6 +4,7 @@ as
 select		tasks.InstanceId
 			,tasks.TaskId
 			,tasks.ExecutionId
+			,r.session_id
 from		sys.dm_exec_requests as r
 cross apply (
 	select try_cast(r.context_info as varchar(128)) as ContextInfo
