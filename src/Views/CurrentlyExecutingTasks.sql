bd5b09bb5cfa16e11b50ca5c2f99cca2cb2623da
Timothy Addison <Timothy.Addison@viagogo.com>
2018-03-22 09:28:01 +0000

    spaces

--- src/Views/CurrentlyExecutingTasks.sql
+++ src/Views/CurrentlyExecutingTasks.sql
@@ -1,19 +1,19 @@
 create or alter view scheduler.CurrentlyExecutingTasks
 as
 
-select		tasks.InstanceId
-			,tasks.TaskId
-			,tasks.ExecutionId
-			,r.session_id
-from		sys.dm_exec_requests as r
+select      tasks.InstanceId
+            ,tasks.TaskId
+            ,tasks.ExecutionId
+            ,r.session_id
+from        sys.dm_exec_requests as r
 cross apply (
-	select try_cast(r.context_info as varchar(128)) as ContextInfo
+    select try_cast(r.context_info as varchar(128)) as ContextInfo
 ) as i
 cross apply openjson (i.ContextInfo, N'$')
-	with (
-		InstanceId		uniqueidentifier	N'$.i'
-		,Taskid			int					N'$.t'
-		,ExecutionId	int					N'$.e'
-	) as tasks
-where	r.context_info <> 0x
-and		isjson(i.ContextInfo) = 1
+    with (
+        InstanceId      uniqueidentifier    N'$.i'
+        ,Taskid         int                 N'$.t'
+        ,ExecutionId    int                 N'$.e'
+    ) as tasks
+where   r.context_info <> 0x
+and     isjson(i.ContextInfo) = 1
