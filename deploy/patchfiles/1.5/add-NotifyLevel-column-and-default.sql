0b1e4f4c5afd779767b424c934b652b3f65a4f2f
Tim Addison <git@tjaddison.com>
2019-11-13 19:19:05 -0500

    remove deployment files

--- deploy/patchfiles/1.5/add-NotifyLevel-column-and-default.sql
+++
@@ -1,9 +0,0 @@
-if col_length(N'scheduler.Task',N'NotifyLevelEventlog') is null
-    alter table scheduler.Task add NotifyLevelEventlog int not null constraint DF_NotifyLevelEventlog default 2
-go
-
-alter table scheduler.Task drop constraint DF_NotifyLevelEventlog;
-go
-
-alter table scheduler.Task add constraint DF_Task_NotifyLevelEventlog default (2) for NotifyLevelEventlog;
-go
--- src/RemoveAllObjects.sql
+++ src/RemoveAllObjects.sql
@@ -2,14 +2,14 @@
 
 /* If system versioning is on turn it off or the drop will fail */
 if exists (
-	select 1
-	from sys.tables as t
-	where t.temporal_type <> 0 
-	and t.name = 'task'
-	and t.schema_id = schema_id('scheduler')
+  select 1
+  from sys.tables as t
+  where t.temporal_type <> 0 
+  and t.name = 'task'
+  and t.schema_id = schema_id('scheduler')
 )
 begin
-	alter table scheduler.task set (system_versioning = off);
+  alter table scheduler.task set (system_versioning = off);
 end
 go
 drop table if exists scheduler.Task;
@@ -44,15 +44,20 @@ go
 declare @dropFun nvarchar(max)=N'';
 
 select @dropFun+='drop function if exists scheduler.'+o.[name]+';'+char(10)
-from sys.objects o 
+from sys.objects as o
 where o.[type] in (N'FN',N'IF',N'TF')
     and o.[schema_id]=schema_id(N'scheduler');
 
 exec sp_executesql @dropFun;
 go
 
+/*** PARTITION FUNCTION ***/
+drop partition function PF_RingBufferByMonthOfYear;
+go
+
 /*** PARTITION SCHEME ***/
 drop partition scheme PS_RingBufferByMonthOfYear;
+go
 
 /*** SCHEMA ***/
 drop schema if exists scheduler;
