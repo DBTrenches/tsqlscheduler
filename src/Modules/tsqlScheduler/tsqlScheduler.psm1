a5d3701fc1f85ebe09ed6fad96081f7983afcf26
Tim Addison <git@tjaddison.com>
2019-11-16 15:50:10 -0500

    add a job class

--- src/Modules/tsqlScheduler/tsqlScheduler.psm1
+++ src/Modules/tsqlScheduler/tsqlScheduler.psm1
@@ -1,4 +1,23 @@
+using module .\Classes\tsqlscheduler.Job.psm1
+
 . $PSScriptRoot/Install-SchedulerSolution
 . $PSScriptRoot/Install-AutoUpsertJob
 . $PSScriptRoot/Publish-TaskFromConfig
 . $PSScriptRoot/Sync-SchedulerState
+
+Function Test-Job
+{
+    [cmdletbinding()]
+    Param (
+
+    )
+    
+      $job = [Job]::new()
+      $otherJob = [Job]::new()
+      $job -eq $otherJob
+      $otherJob.TaskUid = [Guid]::NewGuid()
+      $job -eq $otherJob
+      $job
+      $otherJob
+    
+}
