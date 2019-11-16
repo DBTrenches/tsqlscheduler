0b652b633f9e754dcca237cbc46e930623bfdc23
Tim Addison <git@tjaddison.com>
2019-11-15 19:25:31 -0500

    no more replica status job

--- src/Modules/tsqlScheduler/tsqlScheduler.psm1
+++ src/Modules/tsqlScheduler/tsqlScheduler.psm1
@@ -1,6 +1,5 @@
 . $PSScriptRoot/Install-SchedulerSolution
 . $PSScriptRoot/Install-AutoUpsertJob
-. $PSScriptRoot/Install-ReplicaStatusJob
 . $PSScriptRoot/UnInstall-SchedulerSolution
 . $PSScriptRoot/Publish-TaskFromConfig
 . $PSScriptRoot/Sync-SchedulerState
