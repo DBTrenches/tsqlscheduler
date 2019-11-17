3d4e1be0eeaca752b0f1d3351929eecdbf0a8d14
Tim Addison <git@tjaddison.com>
2019-11-17 10:26:59 -0500

    implement Set-TaskInDatabase

--- src/Modules/tsqlScheduler/Set-TaskInDatabase.ps1
+++ src/Modules/tsqlScheduler/Set-TaskInDatabase.ps1
@@ -12,5 +12,75 @@ Function Set-TaskInDatabase
     [Task] $Task
   )
 
-  Write-Warning "Needs implementation"
+  $mergeQuery = "
+  merge into scheduler.Task as target
+  using (
+    values (
+    '$($Task.TaskUid)'
+    ,'$($Task.Identifier)'
+    ,'$($Task.TSQLCommand)'
+    ,'$($Task.StartTime)'
+    ,'$($Task.Frequency)'
+    ,$($Task.FrequencyInterval)
+    ,'$($Task.NotifyOnFailureOperator)'
+    ,$([int]$Task.IsNotifyOnFailure)
+    ,$([int]$Task.IsEnabled)
+    ,$([int]$Task.IsDeleted)
+    ,'$($Task.NotifyLevelEventLog)'
+    )
+  ) as source (
+    TaskUid,
+    Identifier,
+    TSQLCommand,
+    StartTime,
+    Frequency,
+    FrequencyInterval,
+    NotifyOnFailureOperator,
+    IsNotifyOnFailure,
+    IsEnabled,
+    IsDeleted,
+    NotifyLevelEventlog
+  )
+  on source.TaskUid = target.TaskUid
+  when not matched then insert (
+    TaskUid,
+    Identifier,
+    TSQLCommand,
+    StartTime,
+    Frequency,
+    FrequencyInterval,
+    NotifyOnFailureOperator,
+    IsNotifyOnFailure,
+    IsEnabled,
+    IsDeleted,
+    NotifyLevelEventlog
+  ) values (
+    source.TaskUid,
+    source.Identifier,
+    source.TSQLCommand,
+    source.StartTime,
+    source.Frequency,
+    source.FrequencyInterval,
+    source.NotifyOnFailureOperator,
+    source.IsNotifyOnFailure,
+    source.IsEnabled,
+    source.IsDeleted,
+    source.NotifyLevelEventlog
+  )
+  when matched then update
+    set TaskUid = source.TaskUid,
+      Identifier = source.Identifier,
+      TSQLCommand = source.TSQLCommand,
+      StartTime = source.StartTime,
+      Frequency = source.Frequency,
+      FrequencyInterval = source.FrequencyInterval,
+      NotifyOnFailureOperator = source.NotifyOnFailureOperator,
+      IsNotifyOnFailure = source.IsNotifyOnFailure,
+      IsEnabled = source.IsEnabled,
+      IsDeleted = source.IsDeleted,
+      NotifyLevelEventlog = source.NotifyLevelEventLog
+  ;
+  "
+  
+  Invoke-SqlCmd -ServerInstance $Server -Database $Database -Query $mergeQuery
 }
