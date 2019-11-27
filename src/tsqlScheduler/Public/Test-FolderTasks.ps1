4bbf4ec963b04190929e5fca6a16b5de6be95876
Tim Addison <git@tjaddison.com>
2019-11-26 19:38:46 -0500

    basic correctness testing for a folder

---
+++ src/tsqlScheduler/Public/Test-FolderTasks.ps1
@@ -0,0 +1,54 @@
+Function Test-FolderTasks
+{
+  [cmdletbinding()]
+  Param (
+    [Parameter(Mandatory=$true)]
+    [ValidateScript({ Test-Path -Path $PSItem })]
+    [ValidateNotNullOrEmpty()]
+    [string] $FolderPath
+  )
+
+  $rawFiles = Get-ChildItem -Path $FolderPath
+
+  foreach($rawFile in $rawFiles) {
+    $content = Get-Content -LiteralPath $rawFile -Raw 
+    try {
+      $taskJson = $content | ConvertFrom-Json
+    } catch {
+      Write-Warning "File $rawFile cannot be converted to JSON"
+      continue
+    }
+
+    $requiredProperties = @(
+      "TaskUid",
+      "Identifier",
+      "TSQLCommand",
+      "StartTime",
+      "Frequency",
+      "FrequencyInterval",
+      "NotifyOnFailureOperator",
+      "IsNotifyOnFailure",
+      "IsEnabled",
+      "IsDeleted",
+      "NotifyLevelEventlog"
+    )
+
+    $taskProperties = $taskJson.PSObject.Properties.Name
+  
+    if($null -eq $taskProperties) {
+      Write-Warning "File $rawFile has no properties"
+      continue
+    }
+
+    $missingProperties = Compare-Object -ReferenceObject $requiredProperties -DifferenceObject $taskProperties `
+      | Where-Object -Property SideIndicator -eq "<="
+
+    $missingCount = $missingProperties | Measure-Object | Select-Object -ExpandProperty Count 
+
+    if($missingCount -gt 0) {
+      $missingString = [string]::Join(",", $missingProperties.InputObject)
+      Write-Warning "File $rawFile is missing $missingCount required properties ($missingString)"
+      continue
+    }
+  }
+}
