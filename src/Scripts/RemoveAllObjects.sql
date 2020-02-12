2e1796cb44a88db3684de237acb15d467ebae1d8
Tim Addison <git@tjaddison.com>
2020-02-12 12:15:47 -0500

    tabs to spaces

--- src/Scripts/RemoveAllObjects.sql
+++ src/Scripts/RemoveAllObjects.sql
@@ -2,14 +2,14 @@
 
 /* If system versioning is on turn it off or the drop will fail */
 if exists (
-	select 1
-	from sys.tables as t
-	where t.temporal_type <> 0 
-	and t.name = 'task'
-	and t.schema_id = schema_id('scheduler')
+  select 1
+  from sys.tables as t
+  where t.temporal_type <> 0 
+  and t.name = 'task'
+  and t.schema_id = schema_id('scheduler')
 )
 begin
-	alter table scheduler.task set (system_versioning = off);
+  alter table scheduler.task set (system_versioning = off);
 end
 go
 drop table if exists scheduler.Task;
