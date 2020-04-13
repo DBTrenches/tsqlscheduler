710cb0e05fff7362b983b7adcafe7ffda141374e
Morshed Khan <Morshedk@gmail.com>
2020-04-13 18:05:33 -0400

    double quote the task UID

--- src/tsqlScheduler/SQL/Procedures/SetContextInfo.sql
+++ src/tsqlScheduler/SQL/Procedures/SetContextInfo.sql
@@ -6,8 +6,8 @@ as
 begin
     declare @descriptor varchar(128) = 
 	    '{ "i":"' + cast(@instanceIdentifier as varchar(36)) 
-	    + '","t":' + cast(@taskUid as varchar(36)) 
-	    + ',"e":' + cast(@executionId as varchar(12)) 
+	    + '","t":"' + cast(@taskUid as varchar(36)) 
+	    + '","e":' + cast(@executionId as varchar(12)) 
 	    + '}';
 
     declare @binaryPayload varbinary(128) = cast(@descriptor as varbinary(128));
